ffe36cc71db4c7264611f1f6693cc1c4f4ea67f7
Christopher Kelvin <christopherkelvin100@gmail.com>
2025-01-07 14:47:45 +0300

    Login Page nearly done

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import "./global.css";
+import React from "react";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.blur}></View>
+      <Text style={styles.text}>Login Here</Text>
+      <TextInput placeholder="Enter your email" style={styles.textInput} />
+      <TextInput placeholder="Enter your password" style={styles.textInput} />
+      <Text style={styles.forget}>Don't have an account ?</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff3333",
+    // alignItems: "center",
+    padding: 30,
+    justifyContent: "center",
   },
+  text: {
+    fontSize: 25,
+    textAlign: "left",
+    color: "black",
+    marginBottom: 20,
+    // marginLeft: -200,
+    fontWeight: "bold",
+  },
+  textInput: {
+    backgroundColor: "#F5F5F5",
+    height: 45,
+    width: "100%",
+    opacity: 0.8,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  forget: {
+    marginLeft: 20,
+  },
+  buttonContainer: {
+    display: "flex",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: "#FFDCD1",
+    paddingVertical: 12,
+    paddingHorizontal: 70,
+    borderRadius: 20,
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color:'#FF3333'
+  },
+  blur: {},
 });

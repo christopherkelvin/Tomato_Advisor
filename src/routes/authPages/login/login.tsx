ca1abfeb659628f140c5194fb56827c423d20dcc
Christopher Kelvin <christopherkelvin100@gmail.com>
2025-01-08 19:58:49 +0300

    Key board avoiding

--- src/routes/authPages/login/login.tsx
+++ src/routes/authPages/login/login.tsx
@@ -5,13 +5,17 @@ import {
   TouchableOpacity,
   View,
   Image,
+  KeyboardAvoidingView
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { styles } from "./login.styles";
 import LoaderPage from "../../../components/loaderPage/loaderPage";
+const tomatoImage = require("../../../../assets/Tomato.png");
 export default function Login() {
   const [isLoading, setIsLoading] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
@@ -21,29 +25,34 @@ export default function Login() {
     return <LoaderPage />;
   }
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <View style={styles.blur}>
-        <Image
-          style={styles.blurImage1}
-          source={require("../../../../assets/Tomato.png")}
-        />
+        <Image style={styles.blurImage1} source={tomatoImage} />
       </View>
       <View style={styles.blur1}>
-        <Image
-          style={styles.blurImage1}
-          source={require("../../../../assets/Tomato.png")}
-        />
+        <Image style={styles.blurImage1} source={tomatoImage} />
       </View>
       <Text style={styles.text}>Login Here</Text>
-      <TextInput placeholder="Enter your email" style={styles.textInput} />
-      <TextInput placeholder="Enter your password" style={styles.textInput} />
+      <TextInput
+        placeholder="Enter your email"
+        value={email}
+        style={styles.textInput}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Enter your password"
+        value={password}
+        style={styles.textInput}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
       <Text style={styles.forget}>Don't have an account ?</Text>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} >
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
